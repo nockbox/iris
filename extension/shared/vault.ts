adc2c9b03f9afef09eee39ce8ffdc640db3dff4e
Gohlub <62673775+Gohlub@users.noreply.github.com>
2026-04-29 07:14:36 -0400

    fix: don't block setup response with IO when account setup is initialized msp

--- extension/shared/vault.ts
+++ extension/shared/vault.ts
@@ -2448,8 +2448,7 @@ export class Vault {
       if (existingIndices.has(j)) continue;
 
       const { iconStyleId, iconColor } = this.pickUnusedStyleGlobally();
-      const address =
-        discoveredAddressByIndex.get(j) ?? (await deriveAddress(mnemonic, j));
+      const address = discoveredAddressByIndex.get(j) ?? (await deriveAddress(mnemonic, j));
       const newAccount: SubAccount = {
         name: this.getDefaultChildWalletName(seedOrdinal, j),
         address,
